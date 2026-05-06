d85e1f5088ee5584af49d1e6be95318d05d9e2d1
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-06 11:00:52 +0600

    refactor: empty array specific response in get all category controller

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -22,11 +22,19 @@ const getAllCategory = async (req: Request, res: Response) => {
     try {
         const result = await categoryServices.getAllCategory();
 
-        return res.status(200).json({
-            success: true,
-            message: "Category retrived successfully.",
-            data: result,
-        });
+        if (result.length === 0) {
+            return res.status(200).json({
+                success: true,
+                message: "There is not category yet. Create Some.",
+                data: result,
+            });
+        } else {
+            return res.status(200).json({
+                success: true,
+                message: "Category retrived successfully.",
+                data: result,
+            });
+        }
     } catch (error: any) {
         return res.status(500).json({
             success: false,
