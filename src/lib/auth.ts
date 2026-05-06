ac85b0ebe0cc36706dc2bfe5a55becab2baf8010
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-06 22:18:55 +0600

    adds lib/auth.ts file

---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import {betterAuth} from "better-auth";
+
+export const auth = betterAuth({
+    baseURL: "http://localhost:3000",
+
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+    },
+});
