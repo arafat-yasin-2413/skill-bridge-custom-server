4491c10afcec9f684b4d3f95937cb6148446feaa
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-07 15:37:59 +0600

    add: adds trusted origin, advanced cookie settings and oAuthProxy in lib/auth.ts file

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,9 @@
-import {betterAuth} from "better-auth";
+import { betterAuth } from "better-auth";
+import { oAuthProxy } from "better-auth/plugins";
 
 export const auth = betterAuth({
     baseURL: process.env.FRONTEND_URL,
+    trustedOrigins: [process.env.FRONTEND_URL!],
 
     socialProviders: {
         google: {
@@ -9,4 +11,29 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         },
     },
+
+    advanced: {
+        cookies: {
+            session_token: {
+                name: "session_token",
+                attributes: {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: "none",
+                    partitioned: true,
+                },
+            },
+            state: {
+                name: "session_token",
+                attributes: {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: "none",
+                    partitioned: true,
+                },
+            },
+        },
+    },
+
+    plugins: [oAuthProxy()],
 });
