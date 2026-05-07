fdca78f39e6679342761245b05b76a1707771040
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-07 15:13:42 +0600

    refactor: adds prisma connect, disconnect in server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
-import app from './app';
-import config from './config';
+import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
 async function main() {
-  try {
-    app.listen(config.port, () => {
-      console.log(`Skill Bridge Custom Server is listening on port ${config.port}`);
-    });
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        await prisma.$connect();
+        app.listen(config.port, () => {
+            console.log(
+                `Skill Bridge Custom Server is listening on port ${config.port}`,
+            );
+        });
+    } catch (err) {
+        console.error("Failed to start server: ", err);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
 }
 
 main();
