102e2a7e45be42301799fbca0b1d760fa6175b14
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-07 14:54:41 +0600

    fix: import issues in category.service.ts file

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../../lib/prisma";
+import { prisma } from "../../../lib/prisma";
 
 const createCategory = async (payload: { name: string }) => {
     const result = await prisma.category.create({
