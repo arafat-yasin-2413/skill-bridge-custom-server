49ff0025ebb7f8c8f53e4a26556597dfc724f416
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-07 15:28:20 +0600

    sets origin in cors setup

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,10 @@ const app: Application = express();
 
 // parsers
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}));
 
 // application routes
 // app.use('/api/v1', router);
